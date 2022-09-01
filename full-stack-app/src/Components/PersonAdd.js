d52839dfd6e240a778f04ca3daa9d07142625c7a
orzechovski <87533988+orzechovski@users.noreply.github.com>
2022-09-01 13:07:01 +0200

    Part 6 (6.1-6.2)

--- full-stack-app/src/Components/PersonAdd.js
+++
@@ -1,9 +0,0 @@
-const PersonAdd = ({ name }) => {
-  return (
-    <div style={{ width: `${name.length + 10}ch` }} className="person__new">
-      Added {name}
-    </div>
-  );
-};
-
-export default PersonAdd;
--- full-stack-app/src/Components/ShowError.js
+++
@@ -1,5 +0,0 @@
-const ShowError = ({ error }) => {
-  return <div className="error">{error}</div>;
-};
-
-export default ShowError;
